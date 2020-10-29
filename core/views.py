5a76eb7a0b8466baa963709691394ae7055dd67b
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2020-10-29 15:06:50 +0400

    index view added

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from core.models import User_bids, Category, Brand, Product, Property_name, Property, Product_property
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = Product.objects.order_by('-id')
+        return context
