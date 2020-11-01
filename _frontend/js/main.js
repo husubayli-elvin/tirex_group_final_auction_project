aaec784d4be64464655b9eeff7b6fcccd4d56fbf
Samad Mirzayev <samed.mirzayev@outlook.com>
2020-11-01 16:41:17 +0400

    Added sortable table

--- _frontend/js/main.js
+++ _frontend/js/main.js
@@ -2,4 +2,18 @@
 $(".active-tab").click(function(){
     $('.tab-border-bottom').not(this).removeClass('tab-border-bottom');
     $(this).toggleClass('tab-border-bottom');
- })
+ });
+
+
+//make table sortable
+
+$(document).ready(function () {
+    $('#buying-table').DataTable({
+        searching: false,
+        paging: false,
+        info: false,
+        "language": {
+            "emptyTable": "No Results"
+          }
+        });
+    });
