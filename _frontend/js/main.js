829519bab40bbde20e21bfeefa555ccd1ff1d6e5
Samad Mirzayev <samed.mirzayev@outlook.com>
2020-11-01 14:05:49 +0400

    Added border-bottom to active tabs on buying and selling pages

--- _frontend/js/main.js
+++ _frontend/js/main.js
@@ -1,1 +1,5 @@
-
+// add border-bottom in buying and selling tabs
+$(".active-tab").click(function(){
+    $('.tab-border-bottom').not(this).removeClass('tab-border-bottom');
+    $(this).toggleClass('tab-border-bottom');
+ })
