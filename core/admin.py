9be52d6b5c0238922c853c0e6d7ee9ba6ad31c3a
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2020-11-29 15:51:11 +0400

    Administration page UI changed

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from core.models import User_bids, Category, Brand, Product, Property_name, Property, Product_property, Question
+from django.contrib.auth.models import Group
+
+admin.site.site_header = "Compass"
+admin.site.index_title = "Compass"
+admin.site.site_title = "Compass Administration"
 
 
 admin.site.register([User_bids, Category, Brand, Product, Property_name, Property, Product_property, Question])
+admin.site.unregister(Group)
