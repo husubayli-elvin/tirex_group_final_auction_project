760ce3b507c021a7c1a810dd950e25917cab8c3f
kanan132 <kenan.132@mail.ru>
2020-12-01 00:02:02 +0400

    not completed profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import RegisterView, MainLoginView, ForgetPasswordView, ResetPasswordView
+from .views import RegisterView, MainLoginView, ForgetPasswordView, ResetPasswordView, ProfileView, BuyingView, CopListView, PortfolioView, SellingView, SettingView
 from django.contrib.auth.views import LogoutView
 
 app_name = 'accounts'
@@ -10,5 +10,10 @@ urlpatterns = [
     path('log-out/', LogoutView.as_view(), name='log-out'),
     path('reset-password/', ForgetPasswordView.as_view(), name='reset-password'),
     path('password-reset-confirm/<str:uidb64>/<str:token>/', ResetPasswordView.as_view(), name = 'password-reset-confirm'),
-
+    path('user-profile/<int:pk>', ProfileView.as_view(), name='user-profile'),
+    path('user-profile/<int:pk>/buyings', BuyingView.as_view(), name='buyings'),
+    path('user-profile/<int:pk>/cop-list', CopListView.as_view(), name='cop-list'),
+    path('user-profile/<int:pk>/portfolio', PortfolioView.as_view(), name='portfolio'),
+    path('user-profile/<int:pk>/selling', SellingView.as_view(), name='selling'),
+    path('user-profile/<int:pk>/settings', SettingView.as_view(), name='settings'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from accounts.models import User
+from accounts.models import User, Level
 from django.views.generic import ListView, CreateView, DetailView, TemplateView
 from accounts.forms import RegisterForm, LoginForm, ResetPasswordForm, ResetPasswordConfirmForm
 from django.urls import reverse_lazy
@@ -31,4 +31,58 @@ class ResetPasswordView(PasswordResetConfirmView):
     success_url = reverse_lazy('accounts:login')
   
 
+class ProfileView(DetailView):
+    model = User
+    context_object_name = "profile"
+    template_name = 'account.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class BuyingView(DetailView):
+    model = User
+    context_object_name = "buying"
+    template_name = 'accounts_html/buying.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    
+class CopListView(DetailView):
+    model = User
+    context_object_name = "coplist"
+    template_name = 'accounts_html/cop-list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class PortfolioView(DetailView):
+    model = User
+    context_object_name = "portfolio"
+    template_name = 'accounts_html/portfolio.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+class SellingView(DetailView):
+    model = User
+    context_object_name = "selling"
+    template_name = 'accounts_html/selling.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+class SettingView(DetailView):
+    model = User
+    context_object_name = "setting"
+    template_name = 'accounts_html/settings.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
