961293111cd853af2a56108e8b4b5dfc5aba19ff
Murad Aghazada <muradaghazada@Murads-MacBook-Pro-2.local>
2020-12-13 14:25:33 +0400

    some more detile work

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from core.models import User_bids, Category, Brand, Product, Property_name, Property, Product_property, Question
+from core.models import User_bids, Category, Brand, Product, Property_name, Property, Product_property, Question, Order
 from django.contrib.auth.models import Group
 
 admin.site.site_header = "Compass"
@@ -7,5 +7,5 @@ admin.site.index_title = "Compass"
 admin.site.site_title = "Compass Administration"
 
 
-admin.site.register([User_bids, Category, Brand, Product, Property_name, Property, Product_property, Question])
+admin.site.register([User_bids, Category, Brand, Product, Property_name, Property, Product_property, Question, Order])
 admin.site.unregister(Group)
